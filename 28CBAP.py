c1790464acf73d0fc1305b1f5bf6a59a0ffca926
Mayukh3 <mayukh_d21@iift.edu>
2020-11-28 21:14:36 +0530

    Create 28CBAP.py

---
+++ 28CBAP.py
@@ -0,0 +1,30 @@
+
+print?
+
+print ("Hi Welcome to PA","Welcome to Analytics", sep='-' , end="***")
+print ("Hi Welcome to PA","Welcome to Analytics", sep='-')
+
+!pip install pandas
+
+import pandas
+help(pandas)
+
+import pandas as pd
+
+pd._version_
+
+print ("Hello World")
+
+s1 = input()
+
+print (s1)
+
+s2 = input("Enter Country Name")
+
+print (s2)
+
+print ("My country is ", s2)
+
+s3 = input("Enter Name ")
+
+print ("My country is {0} May name is {1}".format(s2, s3))
